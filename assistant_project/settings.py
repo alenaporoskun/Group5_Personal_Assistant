748e1eeb438ac475d404df1961eb300e68c1f5a1
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2024-09-02 14:05:07 +0300

    Add files via upload

--- assistant_project/settings.py
+++ assistant_project/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
-        'HOST': os.environ.get('DATABASE_DOMAIN'),
     }
 }
 
